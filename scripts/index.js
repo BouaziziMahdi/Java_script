a9784392a8e3cb795a0830d52f6d0f981ded5e19
BouaziziMahdi <mahdibouazizi10@gmail.com>
2024-12-19 12:20:21 +0100

    "primitives types"

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,11 @@
-const h= 45;
+const h= 45.022;
 //js base ala principe essmo dynamiquement typé(les types sont déterminés à l'exécution)
 
 var a="TI"
+var y='m';
+var z= true ;
+var b= null;
+var c;
 console.log(a);
 
 function compare (a,b ){
@@ -26,3 +30,13 @@ function f2(){
 }
 f1();
 f2();
+//primitive types : number, string, boolean, undefined, null
+
+console.log(typeof h);//number
+console.log(typeof a) //string 
+console.log(typeof y) //string 
+console.log(typeof z) //boolean
+console.log(typeof b)//null ( me3niha valeur vide me3nch menha )
+
+console.log(typeof c)//undefined (me3niha ya bro 3andek variable mais ma3andekch valeur)
+g=undefined// rodblek t3eml heka ray temchi ema metjich
